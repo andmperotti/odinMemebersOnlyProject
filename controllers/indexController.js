9a827c18b8cc90d3ff264584f52ca1f209415949
Andrew Perotti <Perotti28@gmail.com>
2026-04-29 08:26:59 -0700

    build: add validation to user registration in indexController
    In the indexController I've added validation via express-validator to ensure that user entry meets the requirements we need their input to match.
    
    build: add required attribute to firstName and lastName as well as minlength
    
    These were not required earlier because I was not sure if they were supposed to be but now reading the project prompt again and working on it I've deemed them required, and a minlength of one has been declared.
    
    build: add errorPage view
    
    This view will display to users how their input did not meet requirements when creating an account.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const dbQueries = require("../db/queries");
-const { body, validationResult } = require("express-validator");
+const { body, validationResult, matchedData } = require("express-validator");
 
 exports.getIndex = (req, res, next) => {
   res.render("index");
@@ -10,28 +10,54 @@ exports.getSignUp = (req, res, next) => {
   res.render("signup");
 };
 
-exports.postSignup = async (req, res, next) => {
-  //hash password
-  //use db query to save user
-  //redirect them to the login page
+//validate user registration entry
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("First name needs to have a minimum length of 1 character")
+    .isAlpha()
+    .withMessage("First name can only contain alphabetic characters"),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage("Last name can only contain alphabetic characters")
+    .isLength({ min: 1 })
+    .withMessage("Last name needs to have a minimum length of 1 character"),
+  body("username").trim().isAlphanumeric().notEmpty("Username cannot be empty"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be atl east 6 characters long"),
+  body("confirmPassword")
+    .isLength({ min: 6 })
+    .withMessage("Password must be atl east 6 characters long"),
+];
 
-  //validate user registration entry
-
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await dbQueries.saveUser(
-      req.body.firstName,
-      req.body.lastName,
-      req.body.username,
-      hashedPassword,
-    );
-    res.redirect("/login");
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-  //later add a check if username is available/ if this user is not already made, if so then reject their request and alert them
-};
+exports.postSignup = [
+  validateUser,
+  async (req, res, next) => {
+    //if there are problems with a users input during registration, navigate them to a page that tells them how their input was bad
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("errorPage", { title: "Create user", errors: errors.array() });
+    }
+    //hash password
+    //use db query to save user
+    //redirect them to the login page
+    try {
+      const { firstName, lastName, username } = matchedData(req);
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      await dbQueries.saveUser(firstName, lastName, username, hashedPassword);
+      res.redirect("/login");
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+    //later add a check if username is available/ if this user is not already made, if so then reject their request and alert them
+  },
+];
 
 exports.getLogin = (req, res, next) => {
   res.render("login");
