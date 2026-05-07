8a06fc090c4c910485fcaf1ccf55a417ae18da3b
Andrew Perotti <Perotti28@gmail.com>
2026-05-07 10:18:03 -0700

    build: import session, passport, localStrategy,etc
    passport module will be used for authentication purposes, session will be used by passport to create sessions store them in our database while creating a related property in the cookies of our users to relate them to stored sessions. localStrategy works with the prior 2 modules to use a username password credential for maintaining user logins on our app/site. pgPool allows us to use connect-pg-simple module to query our database for these session objects.
    
    build: add session middleware
    
    This session middleware will create session objects and relate them to cookies for 30 days.
    
    docs: add explanatory comments
    
    These were used to help myself decipher what middleware was accomplishing which piece of the puzzle.

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 const express = require("express");
-// const passport = require("passport");
-// const session = require("express-session");
 const ejs = require("ejs");
 const path = require("path");
-// const LocalStrategy = require("passport-local").Strategy;
-var bcrypt = require("bcrypt");
-// const connection = require("./db/database");
 const { indexRouter } = require("./routes/index");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const pgPool = require("./db/pool");
 
 //set up app
 const app = express();
 //set up views and view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-//set up for pulling data from forms and body
+//set up for pulling data from forms and body; "parsing http responses"
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//
-// app.use(
-//   session({
-//     store: new (require("connect-pg-simple")(session))({
-//       // Insert connect-pg-simple options here
-//       createTableIfMissing: true,
-//     }),
-//     secret: process.env.COOKIE_SECRET,
-//     resave: false,
-//     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
-//     // Insert express-session options here
-//   }),
-// );
 
-// app.use(passport.session());
+//tell the app to use sessions, if the table doesn't exist create it, otherwise use the COOKIE_SECRET as the hasher, don't resave sessions when nothing is modified, cookies last for 30 days, don't save sessions that are not tied to an account... but still i would think the session table would be created no matter what saveUninitialized's value is set to (it used to be true and still session table was not created)
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      pool: pgPool,
+      //this uses the already created pool that our project is using
+      createTableIfMissing: true,
+      //this initially creates the 'session' table in the database if it doesn't exist yet
+      // Insert connect-pg-simple options here
+    }),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    //for many reasons we'll set this to false, if you need a reminder just read it's description here https://www.npmjs.com/package/express-session
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    // Insert express-session options here
+  }),
+);
 
+//import passport setup?
+require("./config/passport");
+//link sessions to passport strategy
+app.use(passport.session());
+
+//use external router for routes, used after other middleware but before error handler if you have one
 app.use("/", indexRouter);
+
 
 app.listen(process.env.PORT || 3000);
